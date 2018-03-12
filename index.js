0d4cff770af9bc8006ea25cec3c3211321d2d523
Duane Felton <dfelton2010@gmail.com>
2018-03-12 14:50:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+var n = 5;
+
+function inc(n) {
+  
+  n++;
+  return n;
+}
+
+var n = 5;
+
+function dec(n) {
+  
+  n--;
+  return n;
+}
+
+var n = 5;
+
+function makeInt(n) {
+  return n;
+}
+
+function makeInt(n) {
+ return parseInt(n, 10); 
+  
+}
+
